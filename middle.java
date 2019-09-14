683271f3496cc23f2b0cca6dbd8f91f31b109f0e
Yerim-Lee <54611387+Yerim-Lee@users.noreply.github.com>
2019-09-14 14:29:18 +0900

    Add files via upload
    단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
    
    <재한사항>
    s는 길이가 1 이상, 100이하인 스트링입니다.
    
       s	    return
    abcde	 c
    qwer	we

---
+++ middle.java
@@ -0,0 +1,22 @@
+class Solution {
+  public String solution(String s) {
+      String answer = "";
+      
+      if(s.length()%2==1)   // 자리수가 홀수라면 (1,3,5,7,9)
+          answer = s.substring(s.length()/2, s.length()/2 + 1);
+      else                  // 자리수가 짝수라면 (2,4,6,8,10)
+          answer = s.substring(s.length()/2 - 1, s.length()/2 +1);
+    
+      
+      return answer;
+  }
+    
+  public static void main(String[] args)
+  {
+      Solution s = new Solution();
+      
+      String result1 = s.solution("abcde");
+      
+      String result2 = s.solution("qwer");
+  }
+}
